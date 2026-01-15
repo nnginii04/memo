a71839ba2d11dacf447fe520a30089a3602ebfeb
yejin <nnginii040416@gmail.com>
2026-01-15 16:09:57 +0900

    feat : Memo 엔터티 생성

---
+++ src/main/java/com/meta/memo/domain/Memo.java
@@ -0,0 +1,15 @@
+package com.meta.memo.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Memo {
+    private Long id;
+    private String username;
+    private String content;
+
+}
