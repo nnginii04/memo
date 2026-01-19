9dd97ec5315a7142e0dfb5fb0bddb7167ebb79d4
yejin <nnginii040416@gmail.com>
2026-01-19 10:47:24 +0900

    fix : 메모 CRUD 전송 오류 수정 및 DTO 매핑 정리

--- src/main/java/com/meta/memo/dto/MemoResponseDto.java
+++ src/main/java/com/meta/memo/dto/MemoResponseDto.java
@@ -1,19 +1,22 @@
 package com.meta.memo.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.meta.memo.domain.Memo;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
 public class MemoResponseDto {
     private Long id;
+
+    @JsonProperty("username")  // userName 필드를 JSON에서는 username으로 내보냄
     private String userName;
+
     private String contents;
 
     public MemoResponseDto(Memo newMemo) {
-        this.id =  newMemo.getId();
+        this.id = newMemo.getId();
         this.userName = newMemo.getUsername();
         this.contents = newMemo.getContents();
     }
