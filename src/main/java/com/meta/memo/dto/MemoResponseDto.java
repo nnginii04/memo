25d72b5cd2dc6443cba0d58e833a6f747103d7d5
yejin <nnginii040416@gmail.com>
2026-01-19 17:39:08 +0900

    fix: username 필드 불일치로 인한 null 응답 수정

--- src/main/java/com/meta/memo/dto/MemoResponseDto.java
+++ src/main/java/com/meta/memo/dto/MemoResponseDto.java
@@ -1,21 +1,27 @@
 package com.meta.memo.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.meta.memo.domain.Memo;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 public class MemoResponseDto {
     private Long id;
+
+    @JsonProperty("username")
     private String userName;
+
     private String contents;
 
-    public MemoResponseDto(Memo newMemo) {
-        this.id =  newMemo.getId();
-        this.userName = newMemo.getUsername();
-        this.contents = newMemo.getContents();
+    public MemoResponseDto(Memo memo) {
+        this.id = memo.getId();
+        this.userName = memo.getUsername();
+        this.contents = memo.getContents();
     }
 
     public MemoResponseDto(Long id, String username, String contents) {
+        this.id = id;
+        this.userName = username;
+        this.contents = contents;
     }
 }
